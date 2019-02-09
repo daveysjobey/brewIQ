d8062bfb598519f96fb145c525c0805ff68f2e94
Daniel Duffy <dpd1208@gmail.com>
2019-02-09 12:27:38 -0700

    update step 2 and 3 models

--- models/step2.js
+++ models/step2.js
@@ -1,26 +1,150 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Step2 = sequelize.define("Step2", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      body: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1]
-      }
+
+        mash_water_vol: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        strike_temp: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        mash_temp: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        all_in_time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+
+        vorlof_start_time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+
+        lauter_start_time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+
+        sparge_start_time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+
+        sparge_stop_time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+
+        lauter_stop_time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+
+        sparge_water_vol: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        kettle_full_vol: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        preboil_gravity: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        boil_start_time: {
+            type: DataTypes.TIME,
+            allowNull: false,
+        },
+
+        whirlpool_start_time: {
+            type: DataTypes.TIME,
+            allowNull: false,
+        },
+
+        whirlpool_stop_time: {
+            type: DataTypes.TIME,
+            allowNull: false,
+        },
+
+        knock_out_start: {
+            type: DataTypes.TIME,
+            allowNull: false,
+        },
+
+        knock_out_stop: {
+            type: DataTypes.TIME,
+            allowNull: false,
+        },
+
+        knock_out_temp: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        o2_lpm: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        postboil_gravity: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        yeast_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 255]
+            }
+        },
+
+        yeast_gen: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        pitch_vol: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        fv_temp: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        dryhop_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+
+        cellering_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
     });
-  
-    Step2.associate = function(models) {
-      Step2.belongsTo(models.Beer, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
+
+
+    Step2.associate = function (models) {
+        Step2.belongsTo(models.Beer, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
     };
-  
+
     return Step2;
-  };
+};
--- models/step3.js
+++ models/step3.js
@@ -1,26 +1,41 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Step3 = sequelize.define("Step3", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
+
+        bright_tank: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        transfer_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+
+        transfer_vol: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
+        },
+
+        keg_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+
+        keg_vol: {
+            type: DataTypes.DECIMAL(6, 2),
+            allowNull: false,
         }
-      },
-      body: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        len: [1]
-      }
     });
-  
-    Step3.associate = function(models) {
-      Step3.belongsTo(models.Beer, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
+
+    Step3.associate = function (models) {
+        Step3.belongsTo(models.Beer, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
     };
-  
+
     return Step3;
-  };
+};
